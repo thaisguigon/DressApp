3a628ca07d931d83584c846eb2aac28f30581251
Thais Guigon <thaisguigon@yahoo.fr>
2014-03-11 03:26:24 +0100

    Cloth Saving Form

--- DressApp/gen/com/dressapp/R.java
+++ DressApp/gen/com/dressapp/R.java
@@ -8,6 +8,14 @@
 package com.dressapp;
 
 public final class R {
+    public static final class array {
+        /**  String arrays for cloth saving form spinners 
+         */
+        public static final int spinner_colors=0x7f060000;
+        public static final int spinner_occasion=0x7f060002;
+        public static final int spinner_seasons=0x7f060003;
+        public static final int spinner_type=0x7f060001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,12 +32,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int buttonTakeAPicture=0x7f080003;
-        public static final int buttonTakePicture=0x7f080001;
-        public static final int imageView_taken_picture=0x7f080004;
-        public static final int surfaceViewCamera=0x7f080002;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f090011;
+        public static final int button1=0x7f090010;
+        public static final int buttonTakeAPicture=0x7f090003;
+        public static final int buttonTakePicture=0x7f090001;
+        public static final int field_name=0x7f090006;
+        public static final int imageView_taken_picture=0x7f090004;
+        public static final int label_color=0x7f09000b;
+        public static final int label_name=0x7f090005;
+        public static final int label_occasion=0x7f090009;
+        public static final int label_season=0x7f09000e;
+        public static final int label_type=0x7f090007;
+        public static final int spinner_colors1=0x7f09000c;
+        public static final int spinner_colors2=0x7f09000d;
+        public static final int spinner_occasion=0x7f09000a;
+        public static final int spinner_seasons=0x7f09000f;
+        public static final int spinner_type=0x7f090008;
+        public static final int surfaceViewCamera=0x7f090002;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -37,14 +57,27 @@ public final class R {
         public static final int cloth_saving_form=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int button_cloth_form_submit=0x7f05000d;
+        /**  Taken picture strings 
+         */
         public static final int button_take_a_picture=0x7f050005;
         public static final int contentDescription_taken_picture=0x7f050006;
         public static final int hello_world=0x7f050002;
+        public static final int hint_name=0x7f050008;
+        public static final int label_colors=0x7f05000b;
+        /**  Cloth Form labels 
+         */
+        public static final int label_name=0x7f050007;
+        public static final int label_occasion=0x7f05000a;
+        public static final int label_season=0x7f05000c;
+        public static final int label_type=0x7f050009;
+        /**  Display Titles 
+         */
         public static final int title_camera_display=0x7f050003;
         public static final int title_cloth_form=0x7f050004;
     }
@@ -70,10 +103,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
